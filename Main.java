468f4aeed6e9300380531bc9b958ba6b9c576932
Pedro Soares <pedroliv664@gmail.com>
2025-09-16 19:51:47 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    String salaInformada = " ";
+    String salaDesejada = A219;
+    System.out.println("Informe sua sala: ");
+    sala = sc.nextString();
+    
+    if (salaDesejada.equals(salaInformada)){
+    System.out.println("esta é sua sala")
+    } else { System.out.println(" procure sua sala")
+    }
+    sc.close()
+  
+}
